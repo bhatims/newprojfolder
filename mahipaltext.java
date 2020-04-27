b85769f4351d80ef4343235573babca0e198f7a3
edurekamahipal <mahipal@edureka.co>
2020-04-27 15:08:01 +0530

    New file commit updated

---
+++ mahipaltext.java
@@ -0,0 +1,10 @@
+class Demo{
+
+      public static void main(String[] args) {
+
+         System.out.println("This is demo file in Edureka");
+         System.out.println("Second line of the program");
+
+      }
+}
+       
